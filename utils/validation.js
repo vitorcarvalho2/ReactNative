a1d8b97eddb93e8582f7927909d9ab19f6e37939
VitorCarvalho2 <vitorco.oliveira@gmail.com>
2025-02-18 13:52:24 -0300

    verificação de numeros no celular

--- utils/validation.js
+++ utils/validation.js
@@ -9,15 +9,19 @@ export function validateFields(fields) {
 
   if (!fields.cellphone.trim()) {
     errors.cellphone = "Celular é obrigatório.";
+  } else if (!/^\d+$/.test(fields.cellphone)) {
+    errors.cellphone = "Celular deve conter apenas números.";
   } else if (fields.cellphone.length <= 1 || fields.cellphone.length < 10) {
     errors.cellphone = "Celular inválido.";
-  }
+  } 
 
   if (!fields.phone.trim()) {
     errors.phone = "Telefone é obrigatório.";
+  } else if (!/^\d+$/.test(fields.phone)) {
+    errors.phone = "Telefone deve conter apenas números.";
   } else if (fields.phone.length <= 1 || fields.phone.length < 10) {
     errors.phone = "Telefone inválido.";
-  }
+  } 
 
   if (!fields.email.trim()) {
     errors.email = "Email é obrigatório.";
