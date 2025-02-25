0f55c6782c82f78117e148e0fb8b4be3922b68a4
VitorCarvalho2 <vitorco.oliveira@gmail.com>
2025-02-25 15:47:40 -0300

    adição modal para camera ou galeria

--- screens/AddContact/components/Camera.js
+++ screens/AddContact/components/Camera.js
@@ -1,9 +1,15 @@
-import { View, Text, Pressable, StyleSheet,Image, Alert } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  StyleSheet,
+  Image,
+  Alert,
+  Modal,
+} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import styleColors from "../../../assets/static/colors";
-
 import { useState } from "react";
-
 import {
   launchImageLibraryAsync,
   launchCameraAsync,
@@ -12,14 +18,16 @@ import {
   PermissionStatus,
 } from "expo-image-picker";
 
-function Camera({onImagePicked}) {
+function Camera({ onImagePicked }) {
   const [pickedImage, setPickedImage] = useState();
+  const [modalVisible, setModalVisible] = useState(false);
 
   const [cameraPermissionInformation, requestPermission] =
-  useMediaLibraryPermissions();
-  //useCameraPermissions();
+    useCameraPermissions();
+  const [mediaLibraryPermissionInformation, requestMediaLibraryPermission] =
+    useMediaLibraryPermissions();
 
-  async function verifyPermissions() {
+  async function verifyCameraPermissions() {
     if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
       const permitionResponse = await requestPermission();
       return permitionResponse.granted;
@@ -35,29 +43,67 @@ function Camera({onImagePicked}) {
     return true;
   }
 
-  async function getImageHandler() {
-    const hasPermission = await verifyPermissions();
+  async function verifyGalleryPermissions() {
+    if (
+      mediaLibraryPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permitionResponse = await requestMediaLibraryPermission();
+      return permitionResponse.granted;
+    }
 
+    if (mediaLibraryPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to use this app."
+      );
+      return false;
+    }
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyCameraPermissions();
     if (!hasPermission) {
       return;
     }
-    //const image = await launchCameraAsync();
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!image.assets || image.assets.length === 0) {
+      Alert.alert("Erro", "Não foi possível recuperar a imagem.");
+      return;
+    }
+    const pickedImageObject = image.assets[0];
+    setPickedImage(pickedImageObject.uri);
+    onImagePicked(pickedImageObject);
+    setModalVisible(false);
+  }
+
+  async function getImageHandler() {
+    const hasPermission = await verifyGalleryPermissions();
+    if (!hasPermission) {
+      return;
+    }
     const image = await launchImageLibraryAsync({
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
-
-    const imageUri = image.assets?.[0]?.uri;
-
     if (!image.assets || image.assets.length === 0) {
       Alert.alert("Erro", "Não foi possível recuperar a imagem.");
       return;
     }
 
-    const pickedImageObject = image.assets[0]; 
-    setPickedImage(pickedImageObject.uri); 
+    const pickedImageObject = image.assets[0];
+    setPickedImage(pickedImageObject.uri);
     onImagePicked(pickedImageObject);
+    setModalVisible(false);
+  }
+
+  function openModal() {
+    setModalVisible(true);
   }
 
   let image = (
@@ -75,9 +121,45 @@ function Camera({onImagePicked}) {
   return (
     <View style={styles.iconContainer}>
       {image}
-      <Pressable onPress={getImageHandler}>
+      <Pressable onPress={openModal}>
         <Text style={styles.addImageText}>Adicionar Foto</Text>
       </Pressable>
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modal}>
+            <Icon
+              name="close"
+              size={30}
+              color={styleColors.primary100}
+              onPress={() => setModalVisible(false)}
+              style={styles.closeButton}
+            />
+            <Text style={styles.title}>Escolha a Opcao</Text>
+            <View style={styles.buttonContainer}>
+              <Icon
+                name="image-outline"
+                size={30}
+                color={styleColors.primary100}
+                onPress={getImageHandler}
+                style={styles.button}
+              />
+              <Icon
+                name="camera"
+                size={30}
+                color={styleColors.primary100}
+                onPress={takeImageHandler}
+                style={styles.button}
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -106,5 +188,51 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     borderColor: styleColors.primary200,
     borderWidth: 4,
+  },
+  modalOverlay: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modal: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: styleColors.secondary200,
+    padding: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: styleColors.primary100,
+    width: "80%",
+  },
+  title: {
+    color: styleColors.primary100,
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  message: {
+    color: styleColors.primary100,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+    gap: 10,
+  },
+  button: {
+    marginHorizontal: 30,
+    backgroundColor: styleColors.secondary100,
+    borderRadius: 100,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: styleColors.primary100,
+  },
+  closeButton: {
+    position: "absolute",
+    top: 10,
+    right: 10,
   },
 });
