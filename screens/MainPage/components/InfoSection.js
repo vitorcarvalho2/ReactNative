f9f5c89fc4c1c153ef2a897d5f757f42d41ba66a
Enzo Pascoate <enzopascoate@gmail.com>
2025-02-25 20:37:52 -0300

    Alterado tamanho da imagem dem InfoSection

--- screens/MainPage/components/InfoSection.js
+++ screens/MainPage/components/InfoSection.js
@@ -57,8 +57,8 @@ const styles = StyleSheet.create({
       flexDirection: "column",
     },
     image: {
-      width: 60,
-      height: 60,
+      width: 50,
+      height: 50,
       borderRadius: 30,
       borderColor: styleColors.primary200,
     },
